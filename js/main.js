721b2d7b7e58d6191543eddea519b3f700c1b8b5
elpacificmb <elpacificmb@gmail.com>
2020-09-25 16:29:48 +0200

    Single Product Details Page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Js for toggle menu
 
-let menuItems = document.getElementById("menuItems");
+const menuItems = document.getElementById("menuItems");
   menuItems.style.maxHeight = "0px";
 
 function menutoggle(){
@@ -9,4 +9,21 @@ function menutoggle(){
   }else{
     menuItems.style.maxHeight = "0px";
   }
+}
+
+// Js for product gallery
+const productImg = document.getElementById("productImg");
+const smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+  productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+  productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+  productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+  productImg.src = smallImg[3].src;
 }
